63fdf675c646e9db51230ecc92c3d26a81ead04b
bashensanbuqu <64031631+bashensanbuqu@users.noreply.github.com>
2022-04-03 20:52:47 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -63,13 +63,13 @@ fi
       echo -e "\033[35m检测到本机共有$v个IP地址，并成功搭建$s条;多ip服务器游戏推荐使用：方式二\033[0m"
       cat /tmp/s5
       
-      echo -e "\033[33m 是否需要导出所有的配置数据到电脑上？需要请输入 1 ,文件名是 s5 \033[0m"&&read value
-      if [ $value -eq 1 ]; then
+#      echo -e "\033[33m 是否需要导出所有的配置数据到电脑上？需要请输入 1 ,文件名是 s5 \033[0m"&&read value
+#      if [ $value -eq 1 ]; then
             yum -y install lrzsz
             echo -e "\033[41m" 开始导出，请注意文件名是s5 "\033[0m"
-            sz /tmp/s5
-            echo -e "\033[41m" 请注意，文件名是 s5 "\033[0m"
-      fi
+            sz /tmp/s5.txt
+            echo -e "\033[41m" 请注意，文件名是 s5.txt "\033[0m"
+ #     fi
       
       
       echo -e "\033[33m  安装已到位。该脚本仅限内部使用，请勿乱传 \033[0m"&&read -s -n1
@@ -78,7 +78,7 @@ fi
       echo -e "\033[41m安装失败!!! 未知错误 \033[0m"
    fi
 #else 
-#   echo 
+   echo 
 #   echo -e "\033[41m" 模式错误。该工具仅限内部使用 "\033[0m"
 #   echo 
 
