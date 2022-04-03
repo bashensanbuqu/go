20de2cc3aa8e4885b7aee6b0f63e6296bce6ce98
bashensanbuqu <64031631+bashensanbuqu@users.noreply.github.com>
2022-04-04 02:30:48 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -4,7 +4,7 @@
 
 cd /etc/sysconfig/network-scripts
 
-echo 'DEVICE=eth0:0
+echo 'DEVICE=eth0:1
 BOOTPROTO=static
 ONBOOT=yes
 IPADDR=10.0.0.11
@@ -69,15 +69,15 @@ fi
    PIDS=`ps -ef|grep gost|grep -v grep`
    if [ "$PIDS" != "" ]; then
       s=`ps -ef|grep gost|grep -v grep|awk '{print $2}'| wc -l`
-      echo -e "\033[35m检测到本机共有$v个IP地址，并成功搭建$s条;多ip服务器游戏推荐使用：方式二\033[0m"
+      echo -e "\033[35m检测到本机共有$v个IP地址，并成功搭建$s条;多ip服务器游戏推荐使用：中转\033[0m"
       cat /tmp/s5
       
 #      echo -e "\033[33m 是否需要导出所有的配置数据到电脑上？需要请输入 1 ,文件名是 s5 \033[0m"&&read value
 #      if [ $value -eq 1 ]; then
-            yum -y install lrzsz
-            echo -e "\033[41m" 开始导出，请注意文件名是s5 "\033[0m"
-            sz /tmp/s5
-            echo -e "\033[41m" 请注意，文件名是 s5 "\033[0m"
+#            yum -y install lrzsz
+#            echo -e "\033[41m" 开始导出，请注意文件名是s5 "\033[0m"
+#            sz /tmp/s5
+#            echo -e "\033[41m" 请注意，文件名是 s5 "\033[0m"
 #      fi
       
       
@@ -88,7 +88,7 @@ fi
    fi
 #else 
    echo 
-#   echo -e "\033[41m" 模式错误。该工具仅限内部使用 "\033[0m"
-#   echo 
+   echo -e "\033[41m" 模式错误。该工具仅限内部使用 "\033[0m"
+   echo 
 
 #fi
