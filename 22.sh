133cb0e1ab822d6e8bdca82e9c4bb06127132d39
bashensanbuqu <64031631+bashensanbuqu@users.noreply.github.com>
2022-04-04 16:57:56 +0800

    Update 22.sh

--- 22.sh
+++ 22.sh
@@ -102,13 +102,13 @@ if [ "$v" -gt "1" ];then
 #检查用户是否存在，不存在则创建用户
 for i in `seq $v`;
 do
-if id -u sss$i>/dev/null 2>&1; then
+if id -u ssss1>/dev/null 2>&1; then
     echo -e "\033[32m"等待也是一种享受 !!!$i" \033[0m" 
 else
     pw=$(tr -dc "0-9a-zA-Z" < /dev/urandom | head -c 12)> /tmp/log.log;
-    useradd "sss$i"&& echo -e "\033[35m "等待也是一种享受...$i" \033[0m";
-    #echo "$pw" |passwd --stdin sss$i> /tmp/sar.log;
-    #echo "sss$i    ALL=(ALL)    ALL" >> /etc/sudoers  
+    useradd "ssss1"&& echo -e "\033[35m "等待也是一种享受...$i" \033[0m";
+    #echo "$pw" |passwd --stdin ssss1> /tmp/sar.log;
+    #echo "ssss1    ALL=(ALL)    ALL" >> /etc/sudoers  
 fi
 done
 
@@ -124,8 +124,8 @@ done
 for i in `seq $v`;
 do
   if [ ! -n "$pass" ]; then   s5pw=$(tr -dc "0-9a-zA-Z" < /dev/urandom | head -c 8)> /tmp/log.log; else s5pw=$pass; fi
-  echo "su  sss$i -c "\""/usr/local/gost/gost -D -L=sss$i:$s5pw@`sed -n ''$i'p' /tmp/ip.txt`:$port?timeout=30 &"\""">>/etc/rc.d/init.d/ci_gost
-  echo "内转$wip端口$[ $i+10000 ]账号sss$i密码$s5pw	直连`sed -n ''$i'p' /tmp/ip.txt`端口$port账号sss$i密码$s5pw">>/tmp/s5;
+  echo "su  ssss1 -c "\""/usr/local/gost/gost -D -L=ssss1:6666@`sed -n ''$i'p' /tmp/ip.txt`:$port?timeout=30 &"\""">>/etc/rc.d/init.d/ci_gost
+  echo "内转$wip端口$[ $i+10000 ]账号ssss1密码6666	直连`sed -n ''$i'p' /tmp/ip.txt`端口$port账号ssss1密码6666">>/tmp/s5;
 done
 
 #端口映射
@@ -138,8 +138,8 @@ done
 else
   echo -e "\033[33m"单ip出口服务器......" \033[0m" ;
   if [ ! -n "$pass" ]; then   s5pw=$(tr -dc "0-9a-zA-Z" < /dev/urandom | head -c 8)> /tmp/log.log; else s5pw=$pass; fi
-  echo "su  root -c "\""/usr/local/gost/gost -D -L=sss1:$s5pw@$lip:$port?timeout=30 &"\""">>/etc/rc.d/init.d/ci_gost
-  echo "<$wip:$port:sss1:$s5pw>">>/tmp/s5
+  echo "su  root -c "\""/usr/local/gost/gost -D -L=sss1:6666@$lip:$port?timeout=30 &"\""">>/etc/rc.d/init.d/ci_gost
+  echo "<$wip:$port:sss1:6666>">>/tmp/s5
 fi 
 
 if [[ $(iptables-save -t nat) =~ MASQUERADE ]]; then     echo ".."; else     iptables -t nat -A POSTROUTING -j MASQUERADE> /tmp/log.log; fi
