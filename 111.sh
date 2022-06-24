d5ebcb7350633665af2b6734da2942d222252ffe
bashensanbuqu <64031631+bashensanbuqu@users.noreply.github.com>
2022-06-24 22:17:27 +0800

    Delete 111.sh

--- 111.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo sed -i 's/^.*PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
-sudo sed -i 's/^.*PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-echo root:yourRootPasswd | sudo chpasswd
-sudo service ssh restart
-touch 1.sh
