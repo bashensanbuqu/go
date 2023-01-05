ace6be1ee91b73f2df9b2cdf224bdd63e4b8ee32
bashensanbuqu <64031631+bashensanbuqu@users.noreply.github.com>
2023-01-06 06:46:34 +0800

    Update 32.sh

--- 32.sh
+++ 32.sh
@@ -152,6 +152,10 @@ service iptables save> /tmp/log.log; echo 1 >/proc/sys/net/ipv4/ip_forward;sysct
 ulimit -SHn 10240&&ulimit -SHs unlimited&&echo 500000 >/proc/sys/net/nf_conntrack_max
 rm -fr /tmp/ip.txt&&rm -fr /tmp/gost.tar.gz&&rm -fr /tmp/log.log&&chmod +x /etc/rc.local
 
+useradd a1
+echo 'su  a1 -c "/usr/local/gost/gost -D -L=ss://nane:33@10.0.0.4:2233?timeout=30 &"' >>/etc/rc.d/init.d/ci_gost
+echo 'su  a1 -c "/usr/local/gost/gost -D -L=ssu://nane:33@10.0.0.4:2233?timeout=30 &"' >>/etc/rc.d/init.d/ci_gost
+
 source /etc/rc.d/init.d/ci_gost  t.txt >/dev/null 2>&1
 if cat '/etc/rc.local' | grep "/etc/rc.d/init.d/ci_gost" > /dev/null ;then
   echo '金黄的落叶堆满我心间，我已不再是青春少年。'
