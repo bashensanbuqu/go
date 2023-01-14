5b2d44a24dcf063dcc8b096182eacae34068ad00
bashensanbuqu <64031631+bashensanbuqu@users.noreply.github.com>
2023-01-14 15:44:09 +0800

    Create 30.sh

---
+++ 30.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#获取本机非127.0.0的ip个数
+
+echo 'DEVICE=eth0:1
+BOOTPROTO=static
+ONBOOT=yes
+IPADDR=10.0.0.11
+NETMASK=255.255.255.0' >/etc/sysconfig/network-scripts/ifcfg-eth0:1
+echo 'DEVICE=eth0:2
+BOOTPROTO=static
+ONBOOT=yes
+IPADDR=10.0.0.12
+NETMASK=255.255.255.0' >/etc/sysconfig/network-scripts/ifcfg-eth0:2
+/etc/init.d/network restart
+#bash <(curl -s -L https://raw.githubusercontent.com/bashensanbuqu/gos/main/31.sh)  t.txt >/dev/null 2>&1
