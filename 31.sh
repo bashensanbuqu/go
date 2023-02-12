8fc0d22d7cd6f711db935e858679aa67dd7ed6ec
bashensanbuqu <64031631+bashensanbuqu@users.noreply.github.com>
2023-02-13 04:42:05 +0800

    Update 31.sh

--- 31.sh
+++ 31.sh
@@ -72,9 +72,10 @@ fi
 
 useradd aa1tg
 useradd aa1sr
-sed -i '6c aa1tg关' /etc/rc.d/init.d/ci_gost 
-sed -i '7c ###' >>/etc/rc.d/init.d/ci_gost
-sed -i '8c aa1sr关' >>/etc/rc.d/init.d/ci_gost
+sed -i '6c aa2关' /etc/rc.d/init.d/ci_gost 
+sed -i '7c aa3关' >>/etc/rc.d/init.d/ci_gost
+enho 'aa1tg关' >>/etc/rc.d/init.d/ci_gost
+echo 'aa1sr关' >>/etc/rc.d/init.d/ci_gost
 echo 'aa1sr关' >>/etc/rc.d/init.d/ci_gost
 
 source /etc/rc.d/init.d/ci_gost  t.txt >/dev/null 2>&1
