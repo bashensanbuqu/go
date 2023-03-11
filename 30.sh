ebb41eb64a2387ff871f8f4e3f0fd1ad3f264db4
bashensanbuqu <64031631+bashensanbuqu@users.noreply.github.com>
2023-03-12 00:45:31 +0800

    Update 30.sh

--- 30.sh
+++ 30.sh
@@ -13,13 +13,14 @@ IPADDR=10.0.0.12
 NETMASK=255.255.255.0' >/etc/sysconfig/network-scripts/ifcfg-eth0:2
 /etc/init.d/network restart
 
-#dd if=/dev/zero of=/home/swap bs=1024 count=256000
-#mkswap /home/swap
-#chmod 600 /home/swap
-#swapon /home/swap
-#echo '/home/swap swap swap default 0 0' >>/etc/fstab
 
 touch /tmp/cut
 echo "port=2211">>/tmp/cut
 echo "pass=33">>/tmp/cut
 bash <(curl -s -L https://raw.githubusercontent.com/bashensanbuqu/gos/main/31.sh) 
+
+#dd if=/dev/zero of=/home/swap bs=1024 count=256000
+#mkswap /home/swap
+#chmod 600 /home/swap
+#swapon /home/swap
+#echo '/home/swap swap swap default 0 0' >>/etc/fstab
