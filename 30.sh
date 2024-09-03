f462fc3dd89aeeac5ee7cf2ac092e9eeaa8deab4
bashensanbuqu <64031631+bashensanbuqu@users.noreply.github.com>
2024-09-03 17:08:18 +0800

    Update 30.sh

--- 30.sh
+++ 30.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 #获取本机非127.0.0的ip个数
 
-echo 'DEVICE=eth0:1
-BOOTPROTO=static
-ONBOOT=yes
-IPADDR=10.0.0.11
-NETMASK=255.255.255.0' >/etc/sysconfig/network-scripts/ifcfg-eth0:1
-echo 'DEVICE=eth0:2
-BOOTPROTO=static
-ONBOOT=yes
-IPADDR=10.0.0.12
-NETMASK=255.255.255.0' >/etc/sysconfig/network-scripts/ifcfg-eth0:2
-/etc/init.d/network restart
+#echo 'DEVICE=eth0:1
+#BOOTPROTO=static
+#ONBOOT=yes
+#IPADDR=10.0.0.11
+#NETMASK=255.255.255.0' >/etc/sysconfig/network-scripts/ifcfg-eth0:1
+#echo 'DEVICE=eth0:2
+#BOOTPROTO=static
+#ONBOOT=yes
+#IPADDR=10.0.0.12
+#NETMASK=255.255.255.0' >/etc/sysconfig/network-scripts/ifcfg-eth0:2
+#/etc/init.d/network restart
 
 #touch /tmp/cut
 #echo "port=2211">>/tmp/cut
 #echo "pass=33">>/tmp/cut
 #bash <(curl -s -L https://raw.githubusercontent.com/bashensanbuqu/gos/main/31.sh) 
 
-wget -N --no-check-certificate "https://raw.githubusercontent.com/bashensanbuqu/Linux-NetSpeed/master/tcp.sh"
-chmod +x tcp.sh
-./tcp.sh <<<2
+#wget -N --no-check-certificate "https://raw.githubusercontent.com/bashensanbuqu/Linux-NetSpeed/master/tcp.sh"
+#chmod +x tcp.sh
+#./tcp.sh <<<2
 
-dd if=/dev/zero of=/home/swap bs=1024 count=256000
-mkswap /home/swap
-chmod 600 /home/swap
-swapon /home/swap
-echo '/home/swap swap swap default 0 0' >>/etc/fstab
+#dd if=/dev/zero of=/home/swap bs=1024 count=256000
+#mkswap /home/swap
+#chmod 600 /home/swap
+#swapon /home/swap
+#echo '/home/swap swap swap default 0 0' >>/etc/fstab
